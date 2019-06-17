d48a522eee3e863218d3ba8cddb4841ce7266515
Bram Zandbelt <bramzandbelt@gmail.com>
2019-06-17 14:42:52 +0200

    Remove functions masked from irmass
    Note that test_effect_ssd_on_srrt_grp was the only function in stat_functions.R (other stat functions are implemented in notebooks themselves), hence the file is deleted.

--- R/utility_functions.R
+++ R/utility_functions.R
@@ -1,22 +1,5 @@
 # ==============================================================================
 
-#' Check whether directories exists, and create recursively if they don't
-#'
-#' @param all_dirs Character vector of directory paths to check
-#' @export
-check_dir <- function(all_dirs){
-
-  for (this_dir in all_dirs) {
-    if (!dir.exists(this_dir)) {
-      dir.create(this_dir, recursive = TRUE)
-    }
-  }
-
-}
-
-
-# ==============================================================================
-
 #' Read performance data
 #'
 #' Data can be of different types (responses, resp; response times, rt) and can
